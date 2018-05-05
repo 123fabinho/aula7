591f687036e61348833ab2af7aa4df90bd167d03
fabinho123 <fabio19.oliveira@hotmail.com>
2018-05-04 21:32:48 -0300

    primeiro

---
+++ src/app/model/mock-produto.ts
@@ -0,0 +1,15 @@
+
+import { Produto } from "./produto";
+
+export const PRODUTOS: Produto[] = [
+    {id: 1, nome: "um", preco:0.50,imagem:"cinco.jpg"},
+    {id: 1, nome: "dois", preco:0.50,imagem:"dois.jpg"},
+    {id: 1, nome: "tres", preco:0.50,imagem:"um.jpg"} ,
+    {id: 1, nome: "quatro", preco:0.50,imagem:"tres.jpg"},
+    {id: 1, nome: "cinco", preco:0.50,imagem:"quatro.jpg"},
+    {id: 1, nome: "seis", preco:0.50,imagem:"seis.jpg"},
+    {id: 1, nome: "sete", preco:0.50,imagem:"sete.jpg"},
+    {id: 1, nome: "oito", preco:0.50,imagem:"oito.jpg"},
+    {id: 1, nome: "nove", preco:0.50,imagem:"nove.jpg"},
+    {id: 1, nome: "dez", preco:0.50,imagem:"dez.jpg"},
+]
